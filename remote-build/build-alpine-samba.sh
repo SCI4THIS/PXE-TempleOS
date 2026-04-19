c6ae2a5d66609f47d196b07b53d0cffa58243a18
Andrew Kistler <akistler123@gmail.com>
2026-04-19 05:07:56 +0000

    Remove remote-build dir.  Functionality was moved to Samba and nginx dockerfile

--- remote-build/build-alpine-samba.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-docker build \
-  --build-arg UID=$(id -u) \
-  --build-arg GID=$(id -g) \
-  --network=host \
-  -t alpine-samba \
-  .
--- remote-build/run-alpine-samba.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker run --rm --network=host -v ./shared:/shared alpine-samba
