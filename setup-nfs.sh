739610de1fc8ce9385ea994468487a0ebbc1c3a1
Andrew Kistler <akistler123@gmail.com>
2026-05-09 00:57:24 +0000

    escape ()

--- setup-nfs.sh
+++ setup-nfs.sh
@@ -2,7 +2,7 @@
 
 apt install nfs-kernel-server
 
-echo $(pwd)/samba/shared 192.168.1.0/24(rw,sync,no_subtree_check,no_root_squash) >> /etc/exports
+echo "$(pwd)/samba/shared 192.168.1.0/24(rw,sync,no_subtree_check,no_root_squash)" >> /etc/exports
 
 exportfs -rav
 systemctl restart nfs-kernel-server
