19675fdab435134fb92f43716a498616777aaf40
Andrew Kistler <akistler123@gmail.com>
2026-05-09 00:55:47 +0000

    Setup NFS script

---
+++ setup-nfs.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+apt install nfs-kernel-server
+
+echo $(pwd)/samba/shared 192.168.1.0/24(rw,sync,no_subtree_check,no_root_squash) >> /etc/exports
+
+exportfs -rav
+systemctl restart nfs-kernel-server
